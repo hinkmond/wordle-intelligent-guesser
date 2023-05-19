354ba508a27c3d3cffe2f09477a377a23c2196e9
Hinkmond Wong <hinkmond_wong@apple.com>
2023-05-18 20:11:10 -0700

    Fixed clicking of the Play button by using XPath instead of CSS Selector.

--- src/main/java/com/hinkmond/WordleIntelligentGuesser.java
+++ src/main/java/com/hinkmond/WordleIntelligentGuesser.java
@@ -59,9 +59,15 @@ public class WordleIntelligentGuesser {
        // Open browser with desired URL
         driver.get("https://www.nytimes.com/games/wordle/index.html");
 
+        // First, click the "Play" button
+        // XPath for Play button: /html/body/div/div/div/div/div[3]/button[2]
+        WebElement playButton = driver
+                .findElement(By.xpath("/html/body/div/div/div/div/div[3]/button[2]"));
+        playButton.click();
+
+        // Next, click the close "X" button.
         // Selector for Game instructions close "X" button: body > div > div > dialog > div > button > svg
-        WebElement gameIcon = driver.findElement(By.cssSelector("body > div > div > dialog > div > button"));
-        closeIcon = gameIcon.findElement(By.cssSelector("body > div > div > dialog > div > button > svg"));
+        closeIcon = driver.findElement(By.cssSelector("body > div > div > dialog > div > button > svg"));
         closeIcon.click();
 
         // Selector for World App: #wordle-app-game
@@ -76,7 +82,8 @@ public class WordleIntelligentGuesser {
         rootGameApp.click();
 
         String[] firstChoices =
-                {"AISLE", "TEARS", "STALE", "SLIME", "STORE"};
+                {"THYME"};
+                //{"AISLE", "TEARS", "STALE", "ADIOS", "RHYME", "STORE"};
                 //{"AISLE", "TEARS", "REALS", "STALE", "SLIME", "STARE", "STORE"};
                 //{"SLIME"};
         Random random = new Random();
